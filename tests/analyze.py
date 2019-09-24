ba1ca2fdaf1a71ed5d0b3c9004b8b4c1ec65ffaf
glottotopia <sebastian.nordhoff@glottotopia.de>
2019-09-24 22:28:23 +0200

    some computation of time lengths

--- tests/analyze.py
+++ tests/analyze.py
@@ -1,9 +1,12 @@
 import sys
 from lxml import etree
 
-def countVernacularWords(filename):
-  root = etree.parse(filename)
-  print(filename)
+def getTimeslots(root):
+  timeorder = root.find(".//TIME_ORDER")
+  timeslots = dict([(slot.attrib["TIME_SLOT_ID"],slot.attrib["TIME_VALUE"]) for slot in timeorder.findall("TIME_SLOT")])
+  return timeslots
+
+def countVernacularWords(root,timeslots):
   #the LINGUISTIC_TYPE_REF's which contain vernacular sentences
   candidates = ['Text', 'transcription', 'word-txt', 'word', 'UtteranceType']
   for candidate in candidates:   
@@ -17,12 +20,22 @@ def countVernacularWords(filename):
                     for sublist in [av.text.split() 
                                     for av in t.findall(".//ANNOTATION_VALUE")
                                     if av.text!=None] 
-                    for val in sublist]     
-        if wordlist != []:
-          #output the amount found with tier type and ID
-          print("\t%s@%s: %s"%(t.attrib["TIER_ID"],candidate,len(wordlist)))
+                    for val in sublist]          
+        timelist = [(int(timeslots[aa.attrib["TIME_SLOT_REF2"]])-int(timeslots[aa.attrib["TIME_SLOT_REF1"]])) 
+                    for aa in t.findall(".//ALIGNABLE_ANNOTATION")
+                    if aa.find(".//ANNOTATION_VALUE").text!=None
+                    ]     
+        #if wordlist != []:
+        #print(timelist)
+        #print(dict(timeslots))
+        secs = sum(timelist)/1000
+        #output the amount found with tier type and ID
+        print("\t%s@%s: %s (%s s)"%(t.attrib["TIER_ID"],candidate,len(wordlist),secs))
     
 if __name__ == "__main__":
   filename = sys.argv[1]
-  countVernacularWords(filename)
+  print(filename)
+  root = etree.parse(filename)
+  timeslots = getTimeslots(root)
+  countVernacularWords(root,timeslots)
   

39778c4aea2baf21d6022ab7e46553cae8998b8f
glottotopia <sebastian.nordhoff@glottotopia.de>
2019-09-25 16:17:54 +0200

    handle crossref'd timeslots

--- tests/analyze.py
+++ tests/analyze.py
@@ -15,14 +15,32 @@ def getTimeslots(root):
                     ])
   return timeslots
 
-def countVernacularWords(root,timeslots):
+def getAlignableAnnotations(root):
+  """
+  Create a dictionary with time slot ID as keys and offset in ms as values
+  """
+  
+  aas = root.findall(".//ALIGNABLE_ANNOTATION") 
+  d = dict([(aa.attrib["ANNOTATION_ID"],aa) 
+                    for aa in aas 
+                    ]) 
+  return d
+
+def getDuration(annotation):
+  try:
+    return int(timeslots[annotation.attrib["TIME_SLOT_REF2"]])-int(timeslots[annotation.attrib["TIME_SLOT_REF1"]])
+  except AttributeError:
+    return 0
+  
+
+def countVernacularWords(root,timeslots,alignableannotations):
   """
   Retrieve tiers with transcription in an ELAN file. 
   Return the ID of the first tier found, the linguistic type matched, aggregate number of words, aggregate time. 
   """
   
   #the LINGUISTIC_TYPE_REF's which contain vernacular sentences
-  candidates = ['Text', 'transcription', 'word-txt', 'word', 'UtteranceType']
+  candidates = ['Text', 'transcription', 'word-txt', 'interlinear-text-item', 'UtteranceType']
   results = []
   for candidate in candidates:   
     #try different LINGUISTIC_TYPE_REF's to identify the relevant tiers
@@ -37,11 +55,15 @@ def countVernacularWords(root,timeslots):
                                     if av.text!=None] 
                     for val in sublist]  
         #compute a list of durations of each annotation by substracting start times from end times
-        timelist = [(int(timeslots[aa.attrib["TIME_SLOT_REF2"]])-int(timeslots[aa.attrib["TIME_SLOT_REF1"]])) 
+        timelist = [getDuration(aa) 
                     for aa in t.findall(".//ALIGNABLE_ANNOTATION")
                     if aa.find(".//ANNOTATION_VALUE").text!=None
                     ]      
-        secs = sum(timelist)/1000
+        timelistannno = [getDuration(alignableannotations.get(ra.attrib["ANNOTATION_REF"])) 
+                    for ra in t.findall(".//REF_ANNOTATION")
+                    if ra.find(".//ANNOTATION_VALUE").text!=None
+                    ]     
+        secs = sum(timelist+timelistannno)/1000          
         #output the amount found with tier type and ID
         results.append((t.attrib["TIER_ID"],candidate,len(wordlist),secs)) 
   return results  
@@ -61,7 +83,8 @@ if __name__ == "__main__":
     print(filename)
     root = etree.parse(filename)
     timeslots = getTimeslots(root)
-    results = countVernacularWords(root,timeslots) 
+    alignableannotations = getAlignableAnnotations(root)
+    results = countVernacularWords(root,timeslots,alignableannotations) 
     for result in results:
       print("\t%s@%s: %s words (%s seconds)" % result)    
   except IndexError: #no positional argument provided. We analyze all eaf files in directory
@@ -76,7 +99,8 @@ if __name__ == "__main__":
       except KeyError: 
         print("skipping %s (no time slots)" % eaf)
         continue
-      results = countVernacularWords(root,timeslots)
+      alignableannotations = getAlignableAnnotations(root)
+      results = countVernacularWords(root,timeslots,alignableannotations)
       for result in results:
         try:
           globalwords += result[2] #aggregate words 

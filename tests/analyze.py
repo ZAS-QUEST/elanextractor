bbeedda3f55aed1a67a33c233d9e91f9ecc0e327
glottotopia <sebastian.nordhoff@glottotopia.de>
2019-09-25 09:14:34 +0200

    glob files in dirs for processing; some error handling

--- tests/analyze.py
+++ tests/analyze.py
@@ -1,9 +1,14 @@
 import sys
 from lxml import etree
+import glob
+import datetime
+import os 
 
 def getTimeslots(root):
   timeorder = root.find(".//TIME_ORDER")
-  timeslots = dict([(slot.attrib["TIME_SLOT_ID"],slot.attrib["TIME_VALUE"]) for slot in timeorder.findall("TIME_SLOT")])
+  timeslots = dict([(slot.attrib["TIME_SLOT_ID"],slot.attrib["TIME_VALUE"]) 
+                    for slot in timeorder.findall("TIME_SLOT")
+                    ])
   return timeslots
 
 def countVernacularWords(root,timeslots):
@@ -30,12 +35,36 @@ def countVernacularWords(root,timeslots):
         #print(dict(timeslots))
         secs = sum(timelist)/1000
         #output the amount found with tier type and ID
-        print("\t%s@%s: %s (%s s)"%(t.attrib["TIER_ID"],candidate,len(wordlist),secs))
+        return t.attrib["TIER_ID"],candidate,len(wordlist),secs #now returns on first nonzero candidate. #FIXME better check all candidates before  returning
     
 if __name__ == "__main__":
-  filename = sys.argv[1]
-  print(filename)
-  root = etree.parse(filename)
-  timeslots = getTimeslots(root)
-  countVernacularWords(root,timeslots)
+  try:
+    filename = sys.argv[1]
+    print(filename)
+    root = etree.parse(filename)
+    timeslots = getTimeslots(root)
+    result = countVernacularWords(root,timeslots)
+    print("\t%s@%s: %s words (%s seconds)" % result)
+  except IndexError:
+    eafs = glob.glob("./*eaf")
+    globalwords = 0
+    globalsecs = 0
+    for eaf in eafs: 
+      root = etree.parse(eaf)
+      try:
+        timeslots = getTimeslots(root)
+      except KeyError: 
+        print("skipping %s (no time slots)" % eaf)
+        continue
+      result = countVernacularWords(root,timeslots)
+      try:
+        globalwords += result[2]
+        globalsecs += result[3]
+        hours = str(datetime.timedelta(seconds=globalsecs)).split('.')[0]
+      except TypeError:
+        print("skipping %s" % eaf)
+    print("Processed %i files in %s.\n%s transcribed in %i words." % (len(eafs),os.getcwd(),hours, globalwords))
+      
+    
+      
   

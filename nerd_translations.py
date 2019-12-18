597ec3252660bab050484340f40f52f7f6c8db8d
Sebastian Nordhoff <nordhoff@leibniz-zas.de>
2019-12-18 15:07:32 +0100

    pylint

--- nerd_translations.py
+++ nerd_translations.py
@@ -1,42 +1,49 @@
+import sys
+import pprint
 import json
 import requests
-import pprint
-import sys
+
 
 def nerd_text(text):
-    url = 'http://cloud.science-miner.com/nerd/service/disambiguate'
-    if len(text.split(' ')) < 5:
+    """sent text to online resolver and retrieve wikidataId's"""
+
+    url = "http://cloud.science-miner.com/nerd/service/disambiguate"
+    if len(text.split()) < 5:
         return []
-    rtext = requests.post(url, json={'text': text}).text
-    #pprint.pprint(rtext)
-    retrieved_entities = json.loads(rtext).get('entities', [])
-    return list(set([(x['rawName'], x['wikidataId']) 
-            for x in retrieved_entities if x.get('wikidataId')
-           ]))
+    rtext = requests.post(url, json={"text": text}).text
+    # pprint.pprint(rtext)
+    retrieved_entities = json.loads(rtext).get("entities", [])
+    return [(x["rawName"], x["wikidataId"])
+            for x in retrieved_entities
+            if x.get("wikidataId")
+           ]
 
-filename = sys.argv[1]    
-with open(filename) as infile: 
-    jsonfile = json.loads(infile.read())
+if __name__ == "__main__":
+    FILENAME = sys.argv[1]
+    with open(FILENAME) as infile:
+        JSONFILE = json.loads(infile.read())
 
-found_entities = {}
+    FOUND_ENTITIES = {}
 
-LIMIT = 9999999
-OFFSET = 1035
-#OFFSET = 3
-#LIMIT = 4
-for i, key in enumerate(list(jsonfile.keys())[OFFSET:LIMIT]):
-    print(i, key)
-    found_entities[key] = {}
-    for type_ in jsonfile[key]:
-        print(" ", type_)
-        for tier in jsonfile[key][type_]:
-            print("  ", tier)
-            collated_text = ". ".join(jsonfile[key][type_][tier])
-            entities = nerd_text(collated_text)
-            for entity in entities:
-                found_entities[key][entity[1]] = entity[0]
+    LIMIT = 9999999
+    OFFSET = 0
+    # OFFSET = 3
+    # LIMIT = 4
+    for i, key in enumerate(list(JSONFILE.keys())[OFFSET:LIMIT]):
+        print(i, key)
+        FOUND_ENTITIES[key] = {}
+        for type_ in JSONFILE[key]:
+            print(" ", type_)
+            for tier in JSONFILE[key][type_]:
+                print("  ", tier)
+                collated_text = ". ".join(JSONFILE[key][type_][tier])
+                entities = nerd_text(collated_text)
+                for entity in entities:
+                    FOUND_ENTITIES[key][entity[1]] = entity[0]
 
-pprint.pprint(found_entities)
+    #pprint.pprint(FOUND_ENTITIES)
 
-with open('%s_nerd.json'%filename.split('.')[0], 'w') as out:
-    out.write(json.dumps(found_entities, sort_keys=True, indent=4))
+    OUTFILE = "%s_nerd.json" % FILENAME.split(".")[0]
+    with open(OUTFILE, "w") as out:
+        out.write(json.dumps(FOUND_ENTITIES, sort_keys=True, indent=4))
+    print("output is in ", OUTFILE)

ed06f9f9bbb21a383382d7006d8a60522394ce58
Sebastian Nordhoff <nordhoff@leibniz-zas.de>
2020-01-08 09:33:02 +0100

    update blacklist

--- nerd_translations.py
+++ nerd_translations.py
@@ -20,10 +20,28 @@ def nerd_text(text):
             if x.get("wikidataId") and x["wikidataId"] not in blacklist
            ]
 
+# terms which are occasionally recognized, but which are always false positives in the context of ELD
 blacklist = [
     "Q7946755", #'wasn', radio station
     "Q3089073", #'happy, happy', norwegian comedy film
     "Q19893364",#'Inside The Tree', music album
+    "Q49084,"# ss/ short story
+    "Q17646620",# "don't" Ed Sheeran song
+    "Q2261572",# "he/she" Gender Bender
+    "Q35852",# : "ha" hectare
+    "Q119018",#: "Mhm" Mill Hill Missionaries
+    "Q932347",# "gave",# generic name referring to torrential rivers, in the west side of the Pyrenees
+    "Q16836659", #"held" feudal land tenure in England
+    "Q914307",# "ll" Digraph
+    "Q3505473",# "stayed" Stay of proceedings
+    "Q303",# "him/her" Elvis Presley
+    "Q2827398",#: "Aha!" 2007 film by Enamul Karim Nirjhar
+    "Q1477068",# "night and day" Cole Porter song
+    "Q1124888",# "CEDA" Spanish Confederation of the Autonomous Righ
+    
+	
+
+
     ]
 
 if __name__ == "__main__":
